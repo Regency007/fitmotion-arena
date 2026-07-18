db67529bb02649be15679f01810f5b36a9e9b996
Regency007 <kaolnmzae@gmail.com>
2026-07-18 15:37:58 +0700

    Fix MediaPipe loading

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 const MEDIAPIPE_VERSION = "0.10.35";
 const WASM_URL = `https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@${MEDIAPIPE_VERSION}/wasm`;
-const MODULE_URL = `https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@${MEDIAPIPE_VERSION}/+esm`;
+const MODULE_URL = `https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@${MEDIAPIPE_VERSION}/vision_bundle.mjs`;
 const MODEL_URL =
   "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task";
 
